bca8a4af6c81c7fa6851675475143eb1aa200d7f
Coder Klemen <coder.klemen@gmail.com>
2018-04-07 21:32:36 +0200

    continuing with React, Elements, using JS (map) in JSX

--- hackernews/src/index.js
+++ hackernews/src/index.js
@@ -5,4 +5,20 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+/*
+// foolin around
+const element = <h1>Hello, world!</h1>;
+
+ReactDOM.render(
+    element,
+
+    document.getElementById('root')
+);
+*/
+
+registerServiceWorker(); // not strictly needed; used to cache assets
+
+if (module.hot) {
+    module.hot.accept();
+}
